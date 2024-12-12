db873f46a1949812444e1c50d9a2ea9347f5a3b6
OXplakhoa <OXplakhoa@gmail.com>
2024-12-12 12:09:14 +0700

    Use orginal state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name,setName] = useState('')
+  const [submit,setSubmit] = useState(false);
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+  const handleClick = () => {
+    setSubmit(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome</h2>
+      <h2>Welcome {submit ? name : "Guest"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={name} onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
