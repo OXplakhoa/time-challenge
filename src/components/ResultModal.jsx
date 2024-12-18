de9ad12d32892683233e57454dbcc8e05980c5a0
OXplakhoa <OXplakhoa@gmail.com>
2024-12-18 13:10:15 +0700

    Optimize the state

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-export default function ResultModal ({ref,result,target}){
+export default function ResultModal({ ref, result, target }) {
   return (
     <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
-      <p>The target time was <strong>{target} seconds</strong></p>
-      <p>You stopped the timer with <strong>X seconds left</strong></p>
+      <p>
+        The target time was <strong>{target} seconds</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left</strong>
+      </p>
       <form method="dialog">
         <button>Close</button>
       </form>
     </dialog>
   );
-};
+}
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,42 +2,41 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, target }) {
-  const [challenge, setChallenge] = useState({
-    started: false,
-    expired: false,
-  });
   const timer = useRef();
   const dialog = useRef();
-
+  const [timeRemain,setTimeRemain] = useState(target * 1000);
+  const timerActive =  timeRemain > 0 && timeRemain < target * 1000;
+  if(timeRemain <= 0){
+    clearInterval(timer.current);
+    setTimeRemain(target * 1000);
+    dialog.current.showModal();
+  }
   const handleStart = () => {
-    setChallenge((prev) => ({ ...prev, started: true, expired: false }));
-    timer.current = setTimeout(() => {
-      setChallenge((prev) => ({ ...prev, started: false, expired: true }));
-      dialog.current.showModal(); // Show modal when timer expires
-    }, target * 1000);
-  };
-
+    timer.current = setInterval(() => {
+      setTimeRemain(prev => prev - 10);
+    }, 10);
+  }
   const handleStop = () => {
-    clearTimeout(timer.current);
+    dialog.current.showModal();
+    clearInterval(timer.current);
+
   };
 
   return (
     <>
-      {challenge.expired && (
         <ResultModal ref={dialog} target={target} result="lost" />
-      )}
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {target} second{target > 1 && "s"}
         </p>
         <p>
-          <button onClick={challenge.started ? handleStop : handleStart}>
-            {challenge.started ? "Stop" : "Start"} Challenge
+          <button onClick={timerActive ? handleStop : handleStart}>
+            {timerActive? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={challenge.started ? "active" : undefined}>
-          {challenge.started ? "Time is running..." : "Timer inactive"}
+        <p className={timerActive? "active" : undefined}>
+          {timerActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
