4da99411ad483edbe31ee949f65fe549ed351d1c
OXplakhoa <OXplakhoa@gmail.com>
2024-12-19 17:11:37 +0700

    Finish style

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,16 +4,22 @@ export default function ResultModal({ ref, target, remainTime, onReset }) {
   const userLost = remainTime <= 0;
   const formatRemain = (remainTime / 1000).toFixed(2);
   const dialogRef = useRef();
-  useImperativeHandle(ref,() =>{
-    return {
-      open(){
-        dialogRef.current.showModal();
-      }
-    }
-  },[])
+  const score = Math.round((1 - remainTime / (target*1000)) * 100);
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        open() { 
+          dialogRef.current.showModal();
+        },
+      };
+    },
+    []
+  );
   return (
-    <dialog ref={dialogRef} className="result-modal">
+    <dialog ref={dialogRef} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{target} seconds</strong>
       </p>
